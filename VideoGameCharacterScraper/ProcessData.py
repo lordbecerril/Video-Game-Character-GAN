1b11a8f5160beb7629c0232ca0c69a27264ceec6
lordbecerril <becerrilblas@gmail.com>
2020-04-28 03:54:36 -0700

    coo

--- VideoGameCharacterScraper/ProcessData.py
+++ VideoGameCharacterScraper/ProcessData.py
@@ -36,14 +36,14 @@ for file in os.listdir(directory):
     if filename.endswith(".png"):
         print(".png file found")
         print(file.decode("utf-8"))
-        image = PIL.Image.open("./data/"+ str(file.decode("utf-8"))).convert('LA')
+        image = PIL.Image.open("./data/"+ str(file.decode("utf-8")))
         width, height = image.size
         print(width, height)
-        if width != 80:
+        if width != 32:
             #Resize image to 100x100 pixels
-            new_image = image.resize((80,80))
+            new_image = image.resize((32,32))
             image = new_image
-        image.save('../Data/train_data/grayscale_images/'+str(count)+".png")
+        image.save('../Data/train_data/rgb_images/'+str(count)+".png")
         count += 1
         continue
     else:
@@ -58,8 +58,8 @@ for img_name in os.listdir(directory):
     img = Image.open("../Data/train_data/grayscale_images/" + str(img_name.decode("utf-8")))
     rawData = img.load()
     data = []
-    for y in range(80):
-        for x in range(80):
+    for y in range(32):
+        for x in range(32):
             data.append(rawData[x,y][0])
     k = 0
     train_data.loc[i] = [data[k] for k in range(6400)]
